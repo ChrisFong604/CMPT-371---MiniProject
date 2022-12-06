c29003d18c628eaa47431e86e4ba13845362c6a7
ChristopherFong604 <ccffoonngg@gmail.com>
2022-12-05 17:56:38 -0800

    updated server and proxy code

--- ProxyServer/client.py
+++ ProxyServer/client.py
@@ -2,26 +2,22 @@
 from socket import *
 
 # Specify Server Address
-serverName = '10.0.0.62'
-serverPort = 8080
+serverName = '10.118.38.214'
+serverPort = 9000
 
 # Create TCP Socket for Client
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
 # Connect to TCP Server Socket
 clientSocket.connect((serverName,serverPort))
-
-# Recieve user input from keyboard
-sentence = input('Input lowercase sentence:')
-
-# Send! No need to specify Server Name and Server Port! Why?
-clientSocket.send(sentence.encode())
+clientSocket.send("GET /test HTTP/1.1\nHost: 10.118.38.214\nIf-modified-since:2022-12-12".encode())
 
 # Read reply characters! No need to read address! Why?
 modifiedSentence = clientSocket.recv(1024)
 
 # Print out the received string
 print ('From Server:', modifiedSentence.decode())
+input("hahahaha")
 
 # Close the socket
 clientSocket.close()
--- ProxyServer/server1.py
+++ ProxyServer/server1.py
@@ -3,19 +3,23 @@ from socket import *
 from datetime import datetime
 import time
 import os
+
 # Specify Server Port
-serverPort = 8080
+serverPort = 9000
 
 # Create TCP welcoming socket
 serverSocket = socket(AF_INET,SOCK_STREAM)
 
+ipAddress = input("Enter your current device's IPv4 address: ")
+if(ipAddress == ""):
+    ipAddress = '10.118.38.214'
 # Bind the server port to the socket
-serverSocket.bind(('10.0.0.62',serverPort))
+serverSocket.bind((ipAddress,serverPort))
 
 # Server begins listerning foor incoming TCP connections
 serverSocket.listen(1)
 
-print ('--- The server is ready to receive ---')
+print ('--- The MAIN WEB server is ready to receive ---')
 
 try:
 
@@ -23,15 +27,14 @@ try:
         # Server waits on accept for incoming requests.
         # New socket created on return
         connectionSocket, addr = serverSocket.accept()
-        
 
         # Read from socket (but not address as in UDP)
         request = connectionSocket.recv(1024).decode()
         #get headers
         headers = request.split('\n')
         #get file path
-        request_type = headers[0].split()[0]
-        print("request type: " + request_type)
+        splitHeaders = headers[0].split()
+        request_type = splitHeaders[0]
         currentTime = datetime.now()
         dateStr = currentTime.strftime("%c")
 
@@ -42,38 +45,42 @@ try:
                 fin = open(filename[1:] + '.html')
                 content = fin.read()
                 fin.close()
-                print(headers)
-                print(len(headers))
                 if ("If-modified-since:" in headers[2]):
                     modTimeEpoch = os.path.getmtime(filename[1:] + '.html')
                     modTime = time.strftime('%Y-%m-%d', time.localtime(modTimeEpoch))
                     fileMod = modTime.split('-')
                     fileDate = datetime(int(fileMod[0]), int(fileMod[1]), int(fileMod[2]))
-                    print(fileDate)
-                    print(modTime)
                     length = str(len(content))
-                    givenDate = headers[2].split(':')[1]
-                    givenMod = givenDate.split('-')
-                    givenDateObj = datetime(int(givenMod[0]), int(givenMod[1]), int(givenMod[2]))
-                    print(headers)
-                    print(givenDate)
-                    if (fileDate < givenDateObj):
+                    date = headers[2].split(':')[1]
+                    modifiers = date.split('-')
+                    dateObject = datetime(int(modifiers[0]), int(modifiers[1]), int(modifiers[2]))
+                    if (fileDate < dateObject):
                         response = f"HTTP/1.1 304 NOT MODIFIED\r\nDate: {dateStr}"
+                        print("Response: \n" + response)
                         connectionSocket.sendall(response.encode("utf-8"))
                     
                     else:
                         length = str(len(content))
                         response = f"HTTP/1.1 200 OK\r\nlength: {length}\r\nContent-Type: text/html\r\nDate: {dateStr}\r\n\r\n" + content
+                        print("Response: \n" + response)
                         connectionSocket.sendall(response.encode("utf-8"))
 
                 else:
                     length = str(len(content))
                     response = f"HTTP/1.1 200 OK\r\nlength: {length}\r\nContent-Type: text/html\r\nDate: {dateStr}\r\n\r\n" + content
+                    print("Response: \n" + response)
                     connectionSocket.sendall(response.encode("utf-8"))
             except FileNotFoundError:
-                response = f'HTTP/1.0 404 NOT FOUND\r\nContent-Type: text/html\r\nDate: {dateStr}'
-                connectionSocket.sendall(response.encode("utf-8"))
-            
+                clientSocket = socket(AF_INET, SOCK_STREAM)
+
+        elif (request_type == 'PUT' or request_type == 'DELETE' or request_type == 'POST'):
+
+            file = open('error.html')
+            content = file.read()
+            file.close()
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nDate: {dateStr}\r\n\r\n" + content
+            print("Response: \n" + response)
+            connectionSocket.sendall(response.encode("utf-8"))
 
 
         elif (request_type == 'HEAD'):
@@ -82,25 +89,18 @@ try:
                 file = open(filename[1:] + '.html')
                 content = file.read()
                 file.close()
-                
                 length = str(len(content))
-
                 response = f"HTTP/1.1 200 OK\r\nlength: {length}\r\nContent-Type: text/html\r\nDate: {dateStr}\r\n\r\n"
+                print("Response: \n" + response)
                 connectionSocket.sendall(response.encode("utf-8"))
             except FileNotFoundError:
                 response = f'HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html\r\nDate: {dateStr}'
+                print("Response: \n" + response)
                 connectionSocket.sendall(response.encode("utf-8"))
-
-        elif (request_type == 'PUT' or request_type == 'DELETE' or request_type == 'POST'):
-
-            file = open('error.html')
-            content = file.read()
-            file.close()
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nDate: {dateStr}\r\n\r\n" + content
-            connectionSocket.sendall(response.encode("utf-8"))
 
         else:
             response = f'HTTP/1.1 400 BAD REQUEST\r\nDate: {dateStr}'
+            print("Response: \n" + response)
             connectionSocket.sendall(response.encode("utf-8"))
 
         # Send the reply
